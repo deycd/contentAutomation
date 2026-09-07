38f82cf0034c6465b87fed7e179992d2eb51ffcc
dey-ds <abhijit961345@gmail.com>
2026-09-07 20:41:21 +0530

    main.py updated with metadata output

--- src/contentAutomation/main.py
+++ src/contentAutomation/main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from contentautomation!")
-
+from contentAutomation.workflow.structure import VideoMetadata
+from contentAutomation.workflow.pipeline import app
 
 if __name__ == "__main__":
-    main()
+    initial_state = {
+        "video_path": "dem-test.mp4",
+	    "audio_path": None,
+	    "transcript": None,
+	    "metadata": None
+    }
+
+    print(" Starting LangGraph Pipeline Execution...")
+
+    final_output = app.invoke(initial_state)
+        
+    results: VideoMetadata = final_output["metadata"]
+        
+    print("\n================ FINAL OUTPUT ================")
+    print(f"    Title: {results.title}\n")
+    print(f"    Description:\n{results.description}\n")
+    print(f"    Tags: {', '.join(results.tags)}\n")
+    print(f"    Thumbnail Prompt: {results.image_prompt}\n")
+    print("==============================================")
